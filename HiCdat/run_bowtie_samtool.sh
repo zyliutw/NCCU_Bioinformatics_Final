255c2386787dc8715b63415ff7aa19816774dbec
Liu.Z.Y <yad50968@gmail.com>
2017-01-05 16:11:39 +0800

    Update run_bowtie_samtool.sh

--- HiCdat/run_bowtie_samtool.sh
+++ HiCdat/run_bowtie_samtool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for i in $(ls ./Data)
+for i in $(ls ./fasta)
 do
 	bowtie2 --no-unal -x ./fasta_index/build_fasta -U ./fastq/${i} | samtools-1.3.1/samtools view -bS > ./bam/${i%.*}.bam
 done
